cc8ad86e2f0fe54ccf38256fb68cc2ec369b3d2d
William Barrilli <william.barrilli@meta.com.br>
2021-06-23 20:00:51 -0300

    docker-compose final

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, jsonify
 import psycopg2
-import pdb
 
 app = Flask(__name__)
 
 @app.route("/")
 def getUsuarios(): 
-    con = psycopg2.connect(host="0.0.0.0", port='5432', database='chamadosdp', user='postgres', password='postgres')
+    con = psycopg2.connect(host='postgres', port='5432', database='postgres',user='postgres', password='postgres')
     cur = con.cursor()
-    cur.execute('select * from usuarios')
+    cur.execute("SELECT * FROM usuarios")
     recset = cur.fetchall()
     return jsonify(recset)
 
